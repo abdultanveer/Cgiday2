c2c9d4f4e4170968bdd8aafe1ce5949142f0c976
abdul <abdul.tanveer@gmail.com>
2019-08-21 18:17:46 +0530

    runtime permissions

--- app/src/main/java/com/example/cgiday2/SmsActivity.java
+++ app/src/main/java/com/example/cgiday2/SmsActivity.java
@@ -1,7 +1,12 @@
 package com.example.cgiday2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 
@@ -11,9 +16,33 @@ public class SmsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
-        SmsManager manager = SmsManager.getDefault();
-        manager.sendTextMessage("5554",null,
-                "happy birthday",null,null);
+        sendSms();
 
+
+    }
+
+
+    void sendSms(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        1);
+            }
+        }else{         //already has permission granted
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage("5554",null,
+                    "happy birthday",null,null);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
