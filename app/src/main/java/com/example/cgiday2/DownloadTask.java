63a31614437d9dc3b78d676568dcfd6e898a5250
abdul <abdul.tanveer@gmail.com>
2019-08-21 16:37:26 +0530

    onpreexecute and onpostexecute

--- app/src/main/java/com/example/cgiday2/DownloadTask.java
+++ app/src/main/java/com/example/cgiday2/DownloadTask.java
@@ -2,9 +2,10 @@ package com.example.cgiday2;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 
-public class DownloadTask extends AsyncTask<String,Integer,Boolean> {
+public class DownloadTask extends AsyncTask<String,Integer,Boolean> {//inputtype,progresstype,resulttype
     static String TAG = DownloadTask.class.getSimpleName();
 
     ProgressBar progressBar;
@@ -12,25 +13,39 @@ public class DownloadTask extends AsyncTask<String,Integer,Boolean> {
         progressBar = mProgressBar;
     }
 
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
     @Override// this method will execute in the bckground thread//2
     protected Boolean doInBackground(String... strings) {
         Log.i(TAG,"url downloaded = "+strings[0]);
         for(int i=1; i<20;i++){
             try {
-                publishProgress(i);//3
                 Thread.sleep(300);
-                i*=5;
+                publishProgress(new Integer(i*5));//3
+
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
+
         }
         return true;
     }
 
     @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        super.onPostExecute(aBoolean);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
     protected void onProgressUpdate(Integer... i) {
-        super.onProgressUpdate(i);
+        //super.onProgressUpdate(i);
         progressBar.setProgress(i[0]);
     }
 }
