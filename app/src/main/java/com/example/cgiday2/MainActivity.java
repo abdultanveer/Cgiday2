cbfa8031257c594534d266dd7d3c295af651fdc9
abdul <abdul.tanveer@gmail.com>
2019-08-21 17:19:41 +0530

    alarm manager -- sms scheduler

--- app/src/main/java/com/example/cgiday2/MainActivity.java
+++ app/src/main/java/com/example/cgiday2/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.cgiday2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 ProgressBar mProgressBar;
+    DownloadTask downloadTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +20,35 @@ ProgressBar mProgressBar;
     }
 
     public void download(View view) {
-        DownloadTask downloadTask = new DownloadTask(mProgressBar);
-        downloadTask.execute("http://yahoo.com");//1
+        switch (view.getId()){
+            case R.id.buttonDownload:
+                downloadTask = new DownloadTask(mProgressBar);
+                downloadTask.execute("http://yahoo.com");//1
+                break;
+            case R.id.button_sms_schedule:
+                scheduleSms();
+                break;
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //downloadTask.cancel(true);
+    }
+
+    void scheduleSms(){
+        Intent dialIntent = new Intent(MainActivity.this,SmsActivity.class);
+        PendingIntent smsPendingIntent =
+                PendingIntent.getActivity(this,
+                        007,dialIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        long triggerTime = System.currentTimeMillis()+15*1000;
+        int alarmType = AlarmManager.RTC_WAKEUP;
+        AlarmManager alarmManager =
+                (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        alarmManager.setExact(alarmType,triggerTime,smsPendingIntent);
     }
 }
